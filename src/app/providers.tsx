4b8165f86f72d6682ec438773dfd41528fde8ed2
wesleyoliveira303wo-spec <wesleyoliveira303.wo@gmail.com>
2025-11-13 14:44:07 -0300

    Update providers.tsx

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,7 +1,15 @@
 'use client'
 
 import { SessionProvider } from 'next-auth/react'
+import { ThemeProvider } from 'next-themes'
+import { ReactNode } from 'react'
 
-export function Providers({ children }: { children: React.ReactNode }) {
-  return <SessionProvider>{children}</SessionProvider>
+export function Providers({ children }: { children: ReactNode }) {
+  return (
+    <SessionProvider>
+      <ThemeProvider attribute="class" defaultTheme="dark">
+        {children}
+      </ThemeProvider>
+    </SessionProvider>
+  )
 }
