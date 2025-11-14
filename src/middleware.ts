ab840313f0c6efa65e69134d42c63d9ae59c78fe
wesleyoliveira303wo-spec <wesleyoliveira303.wo@gmail.com>
2025-11-14 13:29:51 -0300

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,5 @@
-export { default } from "next-auth/middleware"
+export { auth as middleware } from "next-auth/middleware"
 
 export const config = {
-  matcher: ["/dashboard/:path*", "/settings/:path*"]
+  matcher: ["/dashboard/:path*", "/settings/:path*"],
 }
